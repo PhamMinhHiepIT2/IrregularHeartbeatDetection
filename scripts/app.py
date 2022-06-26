8190551214486847d06c00955e60b10969ac444c
PhamMinhHiep <phamminhhip2000@gmail.com>
2022-06-26 17:59:30 +0700

    update api

--- scripts/app.py
+++ scripts/app.py
@@ -1,10 +1,12 @@
 from tensorflow import keras
 from fastapi import FastAPI
 import uvicorn
-
+import cv2
 
 MODEL_PATH = "model/vgg16.h5"
 app = FastAPI()
+
+CLASSES_TO_CHECK = ['L', 'N', 'V', 'A', 'R']
 
 
 def load_model(model_path: str):
@@ -15,7 +17,16 @@ def load_model(model_path: str):
 
 
 def predict(model, image):
-    return model.predict(image)
+    img = cv2.imread(image)
+    img = cv2.resize(img, (224, 224))
+    img = img.reshape(1,224,224,3)
+    prob = model.predict(img)
+    label_pred = prob.argmax(axis=-1)
+    res = CLASSES_TO_CHECK[label_pred[0]]
+    print("Probability: {}".format(prob))
+    print("Result: {}".format(res))
+    return_val = "Result: {} with probability {}".format(res, prob[0][label_pred[0]])
+    return return_val
 
 
 @app.get("/")
@@ -25,4 +36,4 @@ def image_pred(image_path):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
